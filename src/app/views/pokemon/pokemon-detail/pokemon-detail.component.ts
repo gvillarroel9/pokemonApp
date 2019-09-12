8699fc65b2587df6491b86ebc019fb60f6a7c59b
unknown <gvillarroel9@gmail.com>
2019-09-11 20:59:01 -0400

    Details Evolutions Tab

--- src/app/views/pokemon/pokemon-detail/pokemon-detail.component.ts
+++ src/app/views/pokemon/pokemon-detail/pokemon-detail.component.ts
@@ -27,18 +27,54 @@ import { trigger, style, transition, animate, state  } from '@angular/animations
 export class PokemonDetailComponent implements OnInit {
   pokemonId: string;
   pokemonData: any;
+  evolutions = [];
   constructor(private _pokemonService : PokemonService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.pokemonId = this.route.snapshot.paramMap.get("id");
     this._pokemonService.getPokemon(environment.urlApi+'pokemon/'+this.pokemonId).subscribe((response)=>{
-      console.log(response);
       this.pokemonData = response;
+      this.getSpecies(this.pokemonId);
     },
     (err: any)=>{
       console.log("Error.",err);
     });
+  }
 
+  getSpecies(id: any){
+    this._pokemonService.getPokemonSpecies(id).subscribe((response)=>{
+        this.getEvolution(response['evolution_chain'].url , response['name'], id)
+      },
+      (err: any)=>{
+        console.log("Error.",err);
+      });
+  }
+
+  getEvolution(url: any, pokemon:any, id:any){
+    this._pokemonService.getEvolutionChain(url).subscribe((response)=>{
+      let chain = response['chain'];
+      let evolutionChain = [];
+      while(chain.evolves_to.length){
+        this.getPokemonDetail(chain.species.name);
+        chain = chain.evolves_to[0];        
+      }
+        this.getPokemonDetail(chain.species.name);
+    }, 
+    (err: any)=>{
+      console.log("Error.",err);
+    });
+  }
+
+  getPokemonDetail(pokemonName: string){
+    this._pokemonService.getPokemon(environment.urlApi+'pokemon/'+pokemonName).subscribe((response)=>{
+        this.evolutions.push(response);
+        if(this.evolutions.length === 3){
+          this.evolutions.sort((a,b) => a.order-b.order);
+        }
+      },
+      (err: any)=>{
+        console.log("Error.",err);
+      });
   }
 
 }
